3594518218e15eb26693795a0eb91bc4aeb98cae
srisai_pasupuleti <srisaichiyan24@gmail.com>
2020-06-02 16:33:03 +0530

    rewritten whole code

--- graph_bfs_s.cpp
+++ graph_bfs_s.cpp
@@ -1,55 +1,64 @@
-#include<bits/stdc++.h>
 
+/*  input format 
+    no:of vertices
+    no:of edges
+    input edges
+    then input root node 
+*/
+
+
+#include<bits/stdc++.h>
 using namespace std;
+int addedge(vector<int> adj[],int a,int b){
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+    return 0;
+}
+int showadj(vector<int> adj[],int v){
+    for(int i=0;i<v;i++){
+        cout<<"vertex "<<i;
+        for(int j=0;j<adj[i].size();j++){
+            cout<<"->"<<adj[i][j];
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+void bfs(vector<int> adj[],int s,int v){
+    queue<int> q;
+    q.push(s);
+    bool visited[v]={false};
+    visited[s]=true;
+    while(!q.empty()){
+        int f=q.front();
+        vector<int>::iterator it;
+        for(it=adj[f].begin();it!=adj[f].end();it++){
+            int i=*it;
+            if(visited[i]==false){
+                q.push(i);
+                visited[i]=true;
+            }
+        }
+        cout<<"->"<<f;
+        q.pop();
+    }
+}
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int n,a,b;
-    cin>>n;
-    vector<int>v[n+1];
-    vector<bool>visit(n+1,false);
-    queue<int>q;
-    for(int i=0;i<n-1;i++){
-        cin>>a>>b;
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
-    cin>>a;
-    /*
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int v;cin>>v;
+    vector<int> adj[v];
+    int n;cin>>n;
+    int a,b;
     for(int i=0;i<n;i++){
-        cout<<i<<" ";
-        for(int j=0;j<v[i].size();j++)
-            cout<<v[i][j]<<" ";
-        cout<<endl;
+        cin>>a>>b;
+        addedge(adj,a,b);
     }
-    */
-    q.push(1);
-    int c=0;
-    while(a>1 && !q.empty()){
-        int val=q.front();
-        visit[val]=true;
-        q.pop();
-        int i=0;
-        while(i<v[val].size()){
-            q.push(v[val][i]);
-            i++;
-        }
-        a--;
-    }
-    
-    while(!q.empty()){
-        int val=q.front();
-        if(visit[val]!=true){
-            visit[val]=true;
-            c++;
-        }
-        q.pop();
-    }
-    
-    //c=q.size();
-    cout<<c<<endl;
+    int s;
+    cin>>s;
+    showadj(adj,v);
+    bfs(adj,s,v);
     return 0;
 }
